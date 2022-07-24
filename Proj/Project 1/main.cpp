f2535e902158b5e47ae123d8076e87444d440ea9
Caden Hartshorn <cadenhartshorn@gmail.com>
2022-07-23 23:27:16 -0700

    Update Project 1
    -Upgraded & Finished Shuffling Logic
    -Created Framework for Player Move Logic

--- Proj/Project 1/main.cpp
+++ Proj/Project 1/main.cpp
@@ -1,96 +1,136 @@
 /*
-* file: main.cpp
-* author: caden hartshorn
-* date: July 22 2022 @ 1216
-* purpose: blackjack game
-*/
+ * file: main.cpp
+ * author: caden hartshorn
+ * date: July 22 2022 @ 1216
+ * purpose: blackjack game
+ */
 
-//System libraries
-#include <iostream>
-#include <iomanip>
-#include <vector>
+// System libraries
 #include <cmath>
 #include <fstream>
-#include <time.h>
+#include <iomanip>
+#include <iostream>
 #include <string>
+#include <time.h>
+#include <vector>
 using namespace std;
 
-//User libraries
+// User libraries
 
-//Global Constants - Math/Physics/Conversions Constants
+// Global Constants - Math/Physics/Conversions Constants
 
-//Function Prototypes
+// Function Prototypes
 
-//Execution begins here
-int main(int argc, char** argv) {
-    //seed for the random number generator
-    srand(time(0));
+// Execution begins here
+int main(int argc, char **argv) {
+  // seed for the random number generator
+  srand(time(0));
+
+  // Declare Variables
+  unsigned short numPlys,
+      cCard;               // current card up in deck
+  const short nCards = 52; // number of cards in the deck
+  string face[13] = {"Ace", "2", "3",  "4",    "5",     "6",   "7",
+                     "8",   "9", "10", "Jack", "Queen", "King"};
+  string suit[4] = {"Clubs", "Spades", "Hearts", "Diamonds"};
+  vector<short> p1hand = {};
+  vector<short> p2hand = {};
+  vector<short> dealerhand = {};
+
+  // file stream decl
+  fstream cards;
+  string fileNm;
+
+  // Init Variables & Seek User Input
+  //card init
+  cCard = 0;
   
-    //Declare Variables
-    unsigned short numPlys;
-    const short nCards = 52; // number of cards in the deck
-    string face [13] = {"Ace", "2", "3", "4", "5", "6", "7", "8",
-    "9", "10", "Jack", "Queen", "King"};
-    string suit [4] = {"Clubs", "Spades", "Hearts", "Diamonds"};
-    vector<short> p1hand = {};
-    vector<short> p2hand = {};
-    vector <short> dealerhand = {};
+  // file stream init
+  fileNm = "card.dat";
+  cards.open(fileNm, ios::out);
 
-    //file stream decl
-    fstream cards;
-    string fileNm;
-  
-    //Init Variables & Seek User Input
-    //file stream init
-    fileNm="card.dat";
-    cards.open(fileNm, ios::out);
+  // user vars init
+  numPlys = 3; // 3 players including dealer
 
-    //user vars init
-    numPlys = 3; // 3 players including dealer
-    
-    //Generate Deck Of Cards
-    for (unsigned char i = 0; i < nCards; i++) {
-      string cardStr;
-      cardStr = face[i%13] + "_";
-      cardStr += suit[i/13];
-      cards <<setw(14)<<cardStr<<endl;
-    }
+  // Generate Deck Of Cards
+  for (unsigned char i = 0; i < nCards; i++) {
+    string cardStr;
+    cardStr = face[i % 13] + "_";
+    cardStr += suit[i / 13];
+    cards << setw(14) << cardStr << endl;
+  }
 
-    cards.close();
-  
-    //Map inputs to outputs -> the process
-    //Shuffle Cards
-    cards.open(fileNm, ios::in);
-    vector <string> shfArr;
-    string cLine;
-    while (getline(cards, cLine)) {
-      shfArr.push_back(cLine);
-    }
-    for (short i = 0; i < nCards; i++) {   
-      short rand1 = rand()%nCards+1;
-      short rand2 = rand()%nCards+1;
+  cards.close();
+
+  // Map inputs to outputs -> the process
+  // Shuffle Cards
+  cards.open(fileNm, ios::in);
+  vector<string> shfArr;
+  string cLine;
+  while (getline(cards, cLine)) {
+    shfArr.push_back(cLine);
+  }
+  for (short i = 0; i < nCards; i++) {
+    short rand1 = rand() % nCards + 1;
+    short rand2 = rand() % nCards + 1;
+
+    string swap1 = shfArr[rand1];
+    string swap2 = shfArr[rand2];
+
+    shfArr[rand1] = swap2;
+    shfArr[rand2] = swap1;
+  }
+  cards.close();
+
+  // Generate Game Output
+  cout << right << setw(32) << "BLACKJACK" << endl;
+  cout << right << setw(32) << "2 Player + 1 Dealer Blackjack" << endl;
+  cout << right << setw(32) << "Care to test your odds?" << endl;
+  cout << "******************************" << endl;
+
+  cout << "Enter Any Letter To Continue!" << endl;
+  getchar();
+  cout << endl;
+
+  // Player Turns
+  for (short i = 0; i < numPlys; i++) {
+    bool cntTrn = true;
+
+    // Individual player turn logic
+    do {
+      string hCard1 = shfArr[cCard];
+      cCard++;
+      string hCard2 = shfArr[cCard];
+      cCard++;
+      string hCard3;
       
-      //string swap1 = ;
-      //string swap2= ;
-      
-    }
-    cards.close();
-  
-    //Deal Cards
-    /*for (short i = 0; i < numPlys; i++) {
-      bool cntTrn = true;
-      
-      //Shuffling of Cards
-      do {
-        //generate random card # and see if it has been used in this deck
+      char plyrMv;
 
-        cntTrn = false;
-      } while (cntTrn);
-    }*/
+      if (i == 0 || i == 1) {
+        cout << "Hello Player " << i + 1 << endl;
+        cout << "It is your turn" << endl;
+        cout << "Your hand is:" << endl;
+        cout << hCard1 << endl;
+        cout << hCard2 << endl;
+        cout << "Enter H to Hit and S to Stand" << endl;
+        plyrMv = getchar();
+        cin.ignore();
 
-    //Output Data
-    cout << "Hello world!";
+        if (plyrMv == 'H') {
+          hCard3 = shfArr[cCard];
+          cCard++;
+          cout << "Your 3rd card is " << hCard3 << endl;
+        } else if (plyrMv == 'S') {
+          // stand logic
+        }
+      } else {
+        cout << "Hello Dealer";
+      }
 
-    //Exit stage right!
-    return 0;
+      cntTrn = false;
+    } while (cntTrn);
+  }
+
+  // Exit stage right!
+  return 0;
 }
