73f46fcebf3c07238a1a360bb89a28679ff4f188
Caden Hartshorn <cadenhartshorn@gmail.com>
2022-07-24 11:30:31 -0700

    Update Project 1
    -Introducing Score System Code
    -Fixed a bug relating to user input of move

--- Proj/Project 1/main.cpp
+++ Proj/Project 1/main.cpp
@@ -13,6 +13,7 @@
 #include <string>
 #include <time.h>
 #include <vector>
+#include <cstring>
 using namespace std;
 
 // User libraries
@@ -57,7 +58,7 @@ int main(int argc, char **argv) {
     string cardStr;
     cardStr = face[i % 13] + "_";
     cardStr += suit[i / 13];
-    cards << setw(14) << cardStr << endl;
+    cards << cardStr << endl;
   }
 
   cards.close();
@@ -86,10 +87,11 @@ int main(int argc, char **argv) {
   cout << right << setw(32) << "BLACKJACK" << endl;
   cout << right << setw(32) << "2 Player + 1 Dealer Blackjack" << endl;
   cout << right << setw(32) << "Care to test your odds?" << endl;
-  cout << "******************************" << endl;
+  cout << "********************************" << endl;
 
   cout << "Enter Any Letter To Continue!" << endl;
   getchar();
+  cin.ignore();
   cout << endl;
 
   // Player Turns
@@ -98,31 +100,93 @@ int main(int argc, char **argv) {
 
     // Individual player turn logic
     do {
+      //var declaration
       string hCard1 = shfArr[cCard];
       cCard++;
       string hCard2 = shfArr[cCard];
       cCard++;
-      string hCard3;
+      string hCard3,
+      scrng;
       
       char plyrMv;
+      short score;
+      unsigned char aces;
+
+      //var init
+      score = 0;
+      aces = 0;
 
       if (i == 0 || i == 1) {
         cout << "Hello Player " << i + 1 << endl;
         cout << "It is your turn" << endl;
         cout << "Your hand is:" << endl;
-        cout << hCard1 << endl;
-        cout << hCard2 << endl;
-        cout << "Enter H to Hit and S to Stand" << endl;
-        plyrMv = getchar();
-        cin.ignore();
+        cout << "-" << hCard1 << endl;
+        cout << "-" << hCard2 << endl;
 
-        if (plyrMv == 'H') {
-          hCard3 = shfArr[cCard];
-          cCard++;
-          cout << "Your 3rd card is " << hCard3 << endl;
-        } else if (plyrMv == 'S') {
-          // stand logic
+        //Player Input
+        while (plyrMv != 'H' && plyrMv != 'S') {
+          cout << "Enter H to Hit and S to Stand" << endl;
+          cin >> plyrMv;
+
+          if (plyrMv == 'H') {
+            cout << "Player " << i+1 << " Hits!" << endl;
+            hCard3 = shfArr[cCard];
+            cCard++;
+            cout << "Your 3rd card is: " << endl << hCard3 << endl;
+          } else if (plyrMv == 'S') {
+            // stand logic
+            cout << "Player " << i+1 << " Stands!" << endl;
+          //Player Input Validation
+          } else {
+            cout << "Invalid Input!" << endl;
+          }
         }
+        plyrMv = ' ';
+
+        //Scoring logic
+        //Separate Individual Card Identifiers
+        for (short c = 0; c < 3; c++) {
+          short pos;
+          if (c==0) {
+            pos = hCard1.find("_");
+            scrng = hCard1.substr(0, pos);
+          } else if (c == 1) {
+            pos = hCard2.find("_");
+            scrng = hCard2.substr(0, pos);
+          } else if (c== 2) {
+            if (hCard3.length() > 0) {
+              pos = hCard3.find("_");
+              scrng = hCard3.substr(0, pos);
+            } 
+          }
+
+          if (scrng == "2") score += 2;
+          else if (scrng == "3") score += 3;
+          else if (scrng == "4") score += 4;
+          else if (scrng == "5") score += 5;
+          else if (scrng == "6") score += 6;
+          else if (scrng == "7") score += 7;
+          else if (scrng == "8") score += 8;
+          else if (scrng == "9") score += 9;
+          else if (scrng == "10" || scrng == "Jack" || scrng == "Queen" || scrng == "King") score += 10;
+          else if (scrng == "Ace") aces++;
+          scrng = "";
+        }
+
+        //logic to determine highest value of ace
+        for (short a = 0; a < aces; a++) {
+          if (score+11 <= 21) score += 11;
+          else score++;
+        }
+        
+        cout << "Player " << i+1;
+        if (score > 21) {
+          cout << " Busted!" << endl;
+        } else {
+          cout << " Has a Score of " << score << endl;
+        }
+        score = 0;
+        cout << "-======-" << endl;
       } else {
         cout << "Hello Dealer";
       }
