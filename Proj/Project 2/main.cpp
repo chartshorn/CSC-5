ae9f9d09c0a93111a42db6d9bb9e165384dcedcf
Caden Hartshorn <cadenhartshorn@gmail.com>
2022-07-30 14:47:42 -0700

    Update Project 2 main.cpp
    -Implemented push logic
    -Added additional comments
    =

--- Proj/Project 2/main.cpp
+++ Proj/Project 2/main.cpp
@@ -14,6 +14,7 @@
 #include <string>
 #include <time.h>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 // User libraries
@@ -25,6 +26,7 @@ using namespace std;
 // of length nCards
 string dlCrdH(vector<string> &); // helper - deal an individual card; returns card dealt
 void dlCrd(vector<string> &, string[][6], short); // deal a card
+void sortS(unsigned short [], unsigned short [], short); // sort scores
 void genDeck(string, const short, const string[13],
              const string[4]); // file name, nCards, faces, suits
 void shfDeck(vector<string> &, string, short,
@@ -32,10 +34,10 @@ void shfDeck(vector<string> &, string, short,
 void dlCrds(vector<string> &, string[][6], short); // deck, hands, numPlys
 short score(string [][6], short); // plyHnds, playerid
 void printH(string [][6], short); // plyHnds, playerid
-void printS(unsigned short [], short);
-bool plyrChc(vector<string> &, string[][6], short);
-bool dlrDc(vector<string> &, string[][6], short);
-void scoreGm(unsigned short [], short);
+void printS(unsigned short [], short); // print 1d array of scores
+bool plyrChc(vector<string> &, string[][6], short); // give player choice to hit / stand
+bool dlrDc(vector<string> &, string[][6], short); // automatic dealer decision 
+void scoreGm(unsigned short [], short); // score the game
 
 // Execution begins here
 int main(int argc, char **argv) {
@@ -43,8 +45,8 @@ int main(int argc, char **argv) {
   srand(static_cast<unsigned int>(pow(time(0), 2)));
 
   // Declare Variables
-  const short stdW = 64, maxPlys = 7;
-  unsigned short numPlys;
+  const short stdW = 64, maxPlys = 7; // standard width of header output, max players
+  unsigned short numPlys; // number of players in a game
   const short nCards = 52; // number of cards in the deck
   // arrays
   const string face[13] =
@@ -54,18 +56,17 @@ int main(int argc, char **argv) {
                  "Diamonds"}; // all possible card suits
 
   string plyHnds[maxPlys][6] = {}; // where each player hand will be stored
-  unsigned short scores[maxPlys];
+  unsigned short scores[maxPlys]; // player scores array
 
   // dynamic memory
-  vector<string> deck;
+  vector<string> deck; // shuffled deck
 
   // file stream decl
   string fileNm; // name of the file to read/write to
 
   // Init Variables & Seek User Input
-
   // file stream init
-  fileNm = "card.dat";
+  fileNm = "card.dat"; // generated deck will be loaded and written to this file
 
   // generate deck of cards
   genDeck(fileNm, nCards, face, suit);
@@ -84,6 +85,7 @@ int main(int argc, char **argv) {
   cout << "How many players (excluding the dealer) would you like?" << endl;
   cin >> numPlys;
 
+  //input validation
   while (numPlys < 2 || numPlys > 5) {
     cout << "Invalid number of players! Please enter a range between 2 and 6."
          << endl;
@@ -92,20 +94,20 @@ int main(int argc, char **argv) {
 
   cout << endl;
 
-  // Generate Players
-
   // Deal cards
   dlCrds(deck, plyHnds, numPlys);
 
-  
+  //announce dealers public card
   cout << "The dealer shows card: " << plyHnds[numPlys][1] << endl;
   cout << "-======-" << endl;
 
-  
   // Player Turns - While I is less than the number of players, create a turn
   for (short i = 0; i < numPlys + 1; i++) {
-    bool cntTrn = false;
+    bool cntTrn = false; // whether to continue the players turn
+    //announce current turn
+    cout << (i != numPlys ? "Player " + to_string(i+1) + " " : "Dealer's ") << "Turn";
 
+    //determine blackjack
     if (score(plyHnds, i) == 21) {
       //blackjack
       cout << "Blackjack! ";
@@ -125,7 +127,9 @@ int main(int argc, char **argv) {
       printH(plyHnds, i);
       cout << "For a score of: " << tScore << endl;
 
+      //check if player has busted
       if (tScore < 22) {
+        //if player has not busted, allow them to make a decision
         if (i != numPlys) {
           //Player turn
           cntTrn = plyrChc(deck, plyHnds, i);
@@ -148,9 +152,9 @@ int main(int argc, char **argv) {
   }
 
   cout << "Total Scores:" << endl;
-  printS(scores, numPlys);
+  printS(scores, numPlys); // print list of scores
 
-  scoreGm(scores, numPlys);
+  scoreGm(scores, numPlys); // score the game
   
   // Exit stage right!
   return 0;
@@ -162,6 +166,13 @@ string dlCrdH(vector<string>&deck) {
   return dltCrd;
 }
 
+void swap(int *v1, int *v2) 
+{ 
+    int temp = *v1; 
+    *v1 = *v2; 
+    *v2 = temp; 
+}
+
 void dlCrd(vector<string> &deck, string plyHnds[][6], short player) {
   bool dealt = false;
   for (short i = 2; i < 6; i++) {
@@ -169,6 +180,19 @@ void dlCrd(vector<string> &deck, string plyHnds[][6], short player) {
       dealt = true;
       plyHnds[player][i] = dlCrdH(deck);
     }
+  }
+}
+
+void sortS(unsigned short scores[], unsigned short plyOrdr[], short numPlys) { // sort scores
+  for (short i = 0; i < numPlys; i++) {
+    short minIndx = i;
+    for (short c = i+1; c < numPlys; c++) {
+      if (scores[c] < scores[minIndx]) {
+        minIndx = c;
+      }
+    }
+    swap(scores[minIndx], scores[i]);
+    swap(plyOrdr[minIndx], plyOrdr[i]);
   }
 }
 
@@ -215,11 +239,11 @@ void shfDeck(vector<string> &shfArr, string fileNm, short loops, short nCards) {
 }
 
 void dlCrds(vector<string> &deck, string plyHnds[][6], short numPlys) {
-  // first deal
+  // first pass of table, give each player a card
   for (short i = 0; i < numPlys + 1; i++) {
     plyHnds[i][0] = dlCrdH(deck);
   }
-  //second pass of table
+  //second pass of table, give each player a card
   for (short i = 0; i < numPlys + 1; i++) {
     plyHnds[i][1] = dlCrdH(deck);
   }
@@ -237,7 +261,6 @@ short score(string hands[][6], short player) {
     string scrng;
     pos = hands[player][c].find("_"); // find where the _ in string is
     scrng =  hands[player][c].substr(0, pos); // split string and store first half
-
 
     // calculate card values and add to turn score
     if (scrng == "2")
@@ -280,7 +303,8 @@ short score(string hands[][6], short player) {
   return cScore;
 }
 
-void printH(string plyHnds[][6], short player) {
+void printH(string plyHnds[][6], short player) {  
+  //for each row in array, print each column
   for (short i = 0; i < 6; i++) {
     if (plyHnds[player][i] != "") {
       cout << "-" << plyHnds[player][i] << endl;
@@ -289,10 +313,21 @@ void printH(string plyHnds[][6], short player) {
 }
 
 void printS(unsigned short scores[], short numPlys) {
+  //declare variables
+  unsigned short plyOrdr[numPlys+1];
+  //init variables
+  for (short i = 0; i < numPlys+1; i++) {
+    plyOrdr[i] = i;
+  }
+
+  //sort the scores
+  sortS(scores, plyOrdr, numPlys);
+
+  //loop through each player and display their score
   for (short i = 0; i < numPlys + 1; i++) {
     if (i != numPlys) {
       //Player output
-      cout << "Player " << i+1 << " has a score of: ";
+      cout << "Player " << plyOrdr[i] + 1 << " has a score of: ";
     } else {
       //dealer output
       cout << "Dealer has a score of: ";
@@ -302,30 +337,39 @@ void printS(unsigned short scores[], short numPlys) {
 }
 
 bool plyrChc(vector <string> &deck, string plyHnds[][6], short i) {
-  char plyrMv; // player move
+    char plyrMv; // player input variable
+    //prompt user for input
     cout << "Would you like to hit (H) or stand (S)?" << endl;
     cin >> plyrMv;
+    //standardize input
     plyrMv = toupper(plyrMv);
-    
+
+    //input validation
     while (plyrMv != 'H' && plyrMv != 'S') {
       cout << "Invalid input! Please enter H for hit or S for stand." << endl;
       cin >> plyrMv;
     }
 
+    //the process
+    //evalulate player move
     switch (plyrMv) {
       case 'H': {
+        //player hits
         cout << "Player hits!" << endl;
-        dlCrd(deck, plyHnds, i);
+        dlCrd(deck, plyHnds, i); // deal player addtl card
         return true;
       }
       break;
       case 'S': {
+        //player stands
         cout << "Player stands!" << endl;
         return false;
       }
       break;
       default: {
+        //error handling
         cout << "Critical error (switch.plyrMv)";
+        exit(1);
         return false;
       }
       break;
@@ -333,30 +377,69 @@ bool plyrChc(vector <string> &deck, string plyHnds[][6], short i) {
 }
 
 bool dlrDc(vector<string> &deck, string plyHnds[][6], short i) {
-  short cScore = score(plyHnds, i);
+  //var decl & init
+  short cScore = score(plyHnds, i); // find current score
   if (cScore < 17) {
+    //if dealer has score of less than 17, hit
     cout << "Dealer hits per house rules." << endl;
     dlCrd(deck, plyHnds, i);
     return true;
   } else {
+    //else (score >= 17) stand
     cout << "Dealer stands per house rules." << endl;
     return false;
   }
 }
 
 void scoreGm(unsigned short scores[], short numPlys) {
-  unsigned short maxScr = 0,
-  cWinner = 0;
+  //var decl
+  unsigned short maxScr = 0, //max score
+  cWinner = 0,//current winner
+  pshSc; // push score
+  vector<short> pushers; //how many people are tied
+  
+  //var init
+  bool pshEval = false; //by default noone is pushing
+
+  //the process
+  //loop through every score
   for (short i = 0; i < numPlys + 1; i++) {
-    if (scores[i] > maxScr && scores[i] <= 21) {
+    if (scores[i] >= maxScr && scores[i] <= 21) {
+      //if score is valid and greater than or equal to the previously known max score,
+      if (maxScr == scores[i]) {
+        //if current score element equasl the max score element, players are pushing
+        pshEval = true;
+        pshSc = scores[i];
+      }
+      //set max score equal to current element
       maxScr = scores[i];
       cWinner = i;
     } 
   }
-  if (cWinner != numPlys) {
+
+  if (maxScr == 0) {
+    //everyone busted
+    cout << "All players pushed! (Game tie)" << endl;
+  } else if (pshEval == true && pshSc == maxScr) {
+      //some players pushed
+      cout << "Players:" << endl;
+      for (short i = 0; i < numPlys + 1; i++) {
+        if (scores[i] == maxScr) {
+          if (i == numPlys) {
+            //dealer
+            cout << "Dealer" << endl;
+          } else {
+            cout << "Player " << i+1 << endl;
+          }
+        }
+      }
+      cout << "Pushed! All players not listed lose." << endl;
+  } else if (cWinner != numPlys) {
+    //specific player won
     cout << "Congratulations Player " << cWinner+1 << "!" << endl;
     cout << "They won with a score of " << maxScr << "." << endl;
   } else {
+    //dealer won
     cout << "The house always wins!" << endl;
     cout << "The dealer wins with a score of " << maxScr << "." << endl;
   }
